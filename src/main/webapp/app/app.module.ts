56af5f7795ac0beeaff6521bf892ea6e97a8ab74
Essaid15 <eselbo@hotmail.com>
2020-03-12 03:57:34 -0500

    Revert "add the chat feature to the application"
    This reverts commit 9c02e73033958bb469b89132736d644c8ba8f8b1.

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -14,7 +14,6 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
-import { ChatModule } from './chat/chat.module';
 
 @NgModule({
   imports: [
@@ -24,8 +23,7 @@ import { ChatModule } from './chat/chat.module';
     Mychatbot2HomeModule,
     // jhipster-needle-angular-add-module JHipster will add new module here
     Mychatbot2EntityModule,
-    Mychatbot2AppRoutingModule,
-    ChatModule
+    Mychatbot2AppRoutingModule
   ],
   declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
   bootstrap: [MainComponent]
